46ba8b9ae9adb102dcfb54866776e362e258826a
simondlevy <simon.d.levy@gmail.com>
2021-01-13 21:40:35 -0500

    Support random seed (closes Issue 3)

--- es-test.py
+++ es-test.py
@@ -19,12 +19,14 @@ def main():
     parser.add_argument('filename', metavar='FILENAME', help='input file')
     parser.add_argument('--record', default=None,
                         help='If specified, sets the recording dir')
+    parser.add_argument('--seed', type=int, required=False,
+                        help='Seed for random number generator')
     args = parser.parse_args()
 
     # Load net and environment name from pickled file
     net, env_name = torch.load(open(args.filename, 'rb'))
 
-    print('Total reward = %+.3f ' % eval_net(net, env_name, render=True))
+    print('Total reward = %+.3f ' % eval_net(net, env_name, render=True, seed=args.seed))
 
 
 if __name__ == '__main__':
