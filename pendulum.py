aa26409c0cf7b12ad0f4b43962a3de39821fb536
simondlevy <simon.d.levy@gmail.com>
2021-01-15 00:24:17 -0500

    More pendulum work

--- pendulum.py
+++ pendulum.py
@@ -32,16 +32,15 @@ def eval_net(net, env_name):
     env = gym.make(env_name)
 
     # Run net on environment
-    ob = env.reset()
+    state = env.reset()
     done = False
     total_reward = 0
     steps = 0
 
     while not done:
-        batch = torch.from_numpy(ob[np.newaxis, ...]).float()
-        prediction = net(Variable(batch))
-        action = net(prediction.data.numpy()[0])
-        ob, reward, done, _ = env.step(action)
+        state = torch.FloatTensor(state.reshape(1, -1))
+        action = net(state).data.numpy().flatten()
+        state, reward, done, _ = env.step(action)
         total_reward += reward
         steps += 1
 
@@ -139,9 +138,6 @@ class EvolutionModule:
                 for param in self.weights:
                     x.append(np.random.randn(*param.data.size()))
                 population.append(x)
-
-            print(self.reward_function(self.weights))
-            exit(0)
 
             results = self.pool.map(
                 self.reward_function,
